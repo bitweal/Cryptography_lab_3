f80d74df89d94e36c9fce7ae96f12417e825dd8f
Juuzou Suzuya <bitweal2@gmail.com>
2023-05-26 02:07:33 +0300

    test of smooth

--- without_parallelization.py
+++ without_parallelization.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 def is_prime(num):
     if num < 2:
@@ -16,9 +17,36 @@ def build_factor_base(n, c=3.38):
             factor_base.append(p)
     return factor_base
 
+def is_smooth(alpha, S):
+    k = random.randint(0, n-1)
+    a = alpha**k % n
+    power = []
+    for p in S:
+        c_power = 0
+        while a % p == 0:
+            a //= p        
+            c_power += 1
+        power.append(c_power)
+
+    if all(value == 0 for value in power): 
+        return None
+    else:
+        return power
+    
+def build_linear_equations(alpha, n, S):
+    linear_equations = []
+    for c in range(len(S)+15):
+        smooth_number = is_smooth(alpha, S)
+        if smooth_number == None:
+            continue
+        else:
+            linear_equations.append(smooth_number)
+    return linear_equations
+    
 
 alpha = int(input('Enter alpha: '))
 beta = int(input('Enter beta: '))
 n = int(input('Enter n: '))
 S = build_factor_base(n)
-print(S)
+equation = build_linear_equations(alpha, n, S)
+print(equation)
